f3bdb997fb5f77502ca3db5b5dd43a44a9e5740f
BHAGTANIDEEPAK <bhagatnideepak77@gmail.com>
2025-04-10 17:19:22 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,12 @@ const authRoutes = require('./routes/auth');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+const PORT = process.env.PORT || 5000;
 app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(5000, () => console.log('Server running on port 5000'));
+    app.listen(PORT, () => console.log(`Server running on port ${5000}`));
   })
   .catch(err => console.error(err));

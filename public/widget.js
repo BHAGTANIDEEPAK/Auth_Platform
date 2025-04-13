d7fb6365d2e620729462f4336185a113412ef7d9
BHAGTANIDEEPAK <bhagatnideepak77@gmail.com>
2025-04-13 17:16:54 +0530

    Added otp verification feature

--- public/widget.js
+++ public/widget.js
@@ -1,3 +1,75 @@
+// (function () {
+//     const ORIGIN = window.location.origin;
+  
+//     if (localStorage.getItem(ORIGIN)) return; // already logged in
+  
+//     const style = document.createElement('link');
+//     style.rel = 'stylesheet';
+//     style.href = 'https://auth-platform-gc05.onrender.com/style.css';
+//     document.head.appendChild(style);
+  
+//     const container = document.createElement('div');
+//     container.id = 'auth-widget-container';
+//     document.body.appendChild(container);
+
+//     var login_with_password = false;
+  
+//     fetch('https://auth-platform-gc05.onrender.com/auth.html')
+//       .then(res => res.text())
+//       .then(html => {
+//         container.innerHTML = html;
+  
+//         const formTitle = document.getElementById('form-title');
+//         const toggleLink = document.getElementById('toggle-link');
+//         const authForm = document.getElementById('auth-form');
+  
+//         // ✅ Handle Form Submit
+//         authForm.addEventListener('submit', async (e) => {
+//           e.preventDefault();
+//           const email = document.getElementById('email').value;
+//           const password = document.getElementById('password').value;
+//           const isLogin = formTitle.textContent === 'Login';
+  
+//           const url = `https://auth-platform-gc05.onrender.com/api/auth/${isLogin ? 'login' : 'register'}`;
+  
+//           try {
+//             const res = await fetch(url, {
+//               method: 'POST',
+//               headers: { 'Content-Type': 'application/json' },
+//               body: JSON.stringify({ email, password, projectURL: ORIGIN }),
+//             });
+  
+//             const data = await res.json();
+  
+//             if (res.ok) {
+//               var verify = otpVerify(email);
+//               if(verify){
+//                 localStorage.setItem(ORIGIN, JSON.stringify(data));
+//                 alert('Logged in successfully!');
+//                 location.reload();
+//               }
+//             } else {
+//               alert(data.msg || 'Error');
+//             }
+//           } catch (err) {
+//             console.error('Error:', err);
+//             alert('Network or server error');
+//           }
+//         });
+
+  
+//         // ✅ Handle Toggle
+//         toggleLink.addEventListener('click', function (e) {
+//           e.preventDefault();
+//           const isLogin = formTitle.textContent === 'Login';
+//           formTitle.textContent = isLogin ? 'Register' : 'Login';
+//           toggleLink.textContent = isLogin ? 'Switch to Login' : 'Switch to Register';
+//         });
+//       });
+//   })();
+
+  
+  
 (function () {
     const ORIGIN = window.location.origin;
   
@@ -11,6 +83,8 @@
     const container = document.createElement('div');
     container.id = 'auth-widget-container';
     document.body.appendChild(container);
+
+    var login_with_password = false;
   
     fetch('https://auth-platform-gc05.onrender.com/auth.html')
       .then(res => res.text())
@@ -40,9 +114,12 @@
             const data = await res.json();
   
             if (res.ok) {
-              localStorage.setItem(ORIGIN, JSON.stringify(data));
-              alert('Logged in successfully!');
-              location.reload();
+              var verify = otpVerify(email);
+              if(verify){
+                localStorage.setItem(ORIGIN, JSON.stringify(data));
+                alert('Logged in successfully!');
+                location.reload();
+              }
             } else {
               alert(data.msg || 'Error');
             }
@@ -51,6 +128,7 @@
             alert('Network or server error');
           }
         });
+
   
         // ✅ Handle Toggle
         toggleLink.addEventListener('click', function (e) {
@@ -61,4 +139,6 @@
         });
       });
   })();
+
+  
   
